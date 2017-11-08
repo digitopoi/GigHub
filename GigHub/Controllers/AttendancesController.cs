a8a6e030b73bccba5186a64a004ac88d0d9fd5d4
digitopoi <hubo85@gmail.com>
2017-11-07 20:56:40 -0600

    Implement attend api method

--- GigHub/Controllers/AttendancesController.cs
+++ GigHub/Controllers/AttendancesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +9,29 @@ using System.Web.Http;
 
 namespace GigHub.Controllers
 {
+    [Authorize]
     public class AttendancesController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int gigId)
+        {
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }

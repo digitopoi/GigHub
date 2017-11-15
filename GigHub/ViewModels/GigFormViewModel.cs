773507854318c7cf31f97ea62c1383796766085e
digitopoi <hubo85@gmail.com>
2017-11-15 17:54:24 -0600

    More refactoring

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -7,6 +7,7 @@ namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        public int Id { get; set; }
         [Required]
         public string Venue { get; set; }
 
@@ -24,6 +25,14 @@ namespace GigHub.ViewModels
         public IEnumerable<Genre> Genres { get; set; }
 
         public string Heading { get; set; }
+
+        public string Action
+        {
+            get
+            {
+                return (Id != 0 ? "Update" : "Create");
+            }
+        }
 
         public DateTime GetDateTime()
         {
